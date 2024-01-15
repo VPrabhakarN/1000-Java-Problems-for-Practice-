9c3b0f658846541abfc6a2c76117aed94c5a5673
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-01-15 08:59:50 +0530

    Add files via upload
    This is the 11th program of the 11th problem of an array with solution code.

---
+++ Q11_Array_Problem_11.java
@@ -0,0 +1,12 @@
+// Q.11) Java Program to print the number of elements present in an array 
+
+public class Q11_Array_Problem_11 {
+    public static void main(String args[])
+    {
+        // Creating an array 
+        int arr[] = {12,11,10,9,8,7,6,5,4,3,2,1,0};
+
+        // Printing the number of elements in an array 
+        System.out.print("The number of an elements present in array : "+ arr.length);
+    }
+}
