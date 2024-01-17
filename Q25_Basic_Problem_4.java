948bcc014099e97fe9dfbb64b731553595a50ded
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-01-17 21:05:15 +0530

    Add files via upload
    This is the 25th program of the 4th basic problem.

---
+++ Q25_Basic_Problem_4.java
@@ -0,0 +1,31 @@
+// Q.24) Armstrong Number in Java
+
+import java.util.*;
+public class Q24_Basic_Problem_4 {
+    public static void main(String args[])
+    {
+        //create instance for scanner class
+        Scanner s = new Scanner(System.in);
+        
+        //now take input from user 
+        System.out.println("Enter a number : ");
+        String reverse =" ";
+        String num = s.nextLine();
+        int length = num.length();
+
+        //using for loop
+        for(int i = length-1; i>=0; i--)
+        {
+        reverse = reverse+num.charAt(i);
+        }
+
+        if(num.equals(reverse))
+        {
+            System.out.println("The entered string "+ num+" is a palindrome");
+        }
+        else 
+        {
+            System.out.println("The entered string "+ num+" is not a palindrome");
+        }
+    }
+}
