7130fed59942e4d647c0123f772be971c33892f2
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-02-22 10:12:49 +0530

    Add files via upload
    This is the 46th problem with the solution code.

---
+++ Q46_conversion_problem_7.java
@@ -0,0 +1,16 @@
+// Java program to Control long to String
+
+public class Q46_conversion_problem_7 {
+    public static void main(String args[])
+    {
+        // long 
+        long l = 19231923;
+        System.out.println("Long : "+l);
+
+        // converion 
+        String str = Long.toString(l);
+        System.out.println("String : "+str);
+
+
+    }
+}
