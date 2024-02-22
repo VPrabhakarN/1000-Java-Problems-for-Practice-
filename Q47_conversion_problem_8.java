6a67422df829b83bce525388c0258748ffd5e433
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-02-22 10:13:19 +0530

    Add files via upload
    This is the 47th problem with the solution code.

---
+++ Q47_conversion_problem_8.java
@@ -0,0 +1,13 @@
+// Java program to Convert int to long
+public class Q47_conversion_problem_8 {
+    public static void main(String args[])
+    {
+        // int 
+        int i = 1923;
+        System.out.println("Int : "+i);
+
+        // conversion
+        long l = i;
+        System.out.println("Long : "+l);
+    }
+}
