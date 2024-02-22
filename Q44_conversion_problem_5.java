4c5d52ff7e57ca81215be8bc22103a21b6ba5ab2
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-02-22 10:05:24 +0530

    Add files via upload
    This is the 44th problem with the solution code.

---
+++ Q44_conversion_problem_5.java
@@ -0,0 +1,12 @@
+// Java program to Convert Decimal to Binary
+
+public class Q44_conversion_problem_5 {
+    public static void main(String args[])
+    {
+       // conversion 
+       System.out.println(Integer.toBinaryString(19));
+       System.out.println(Integer.toBinaryString(23));
+       System.out.println(Integer.toBinaryString(3));
+       System.out.println(Integer.toBinaryString(5));  
+    }
+}
