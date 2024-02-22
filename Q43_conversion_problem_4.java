43a3fbcff2292fb8a00f5eb9ebe6f9267f5942a7
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-02-22 10:04:10 +0530

    Add files via upload
    This is the 43th problem with the solution code.

---
+++ Q43_conversion_problem_4.java
@@ -0,0 +1,13 @@
+// Java program to Convert int to char
+
+public class Q43_conversion_problem_4{
+    public static void main(String args[])
+    {
+        // defining an int value 
+        int vita = 84;
+
+        // conversion 
+        char ch = (char) vita;
+        System.out.println(ch);
+    }
+}
