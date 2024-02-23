0050e822845796108813e12e38b7609a5cadad98
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-02-23 21:43:35 +0530

    Add files via upload
    This is 48th problem with solution code.

---
+++ Q48_conversion_problem_9.java
@@ -0,0 +1,13 @@
+// Java program to Convert String to double
+public class Q48_conversion_problem_9 {
+    public static void main(String args[])
+    {
+        // Defing string 
+        String str = "19.23";
+        System.out.println("String : "+str);
+
+        // conversion 
+        double d = Double.parseDouble(str);
+        System.out.println("Double : "+d);
+    }
+}
