970688b0a595baaa380e024d8303dd8498a4a4ac
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-02-24 09:34:17 +0530

    Add files via upload
    This is the 53th problem with the solution code.

---
+++ Q52_conversion_problem_13.java
@@ -0,0 +1,13 @@
+// Java program to Convert Celsius into Fahrenheit
+
+public class Q52_conversion_problem_13 {
+    public static void main(String args[])
+    {
+        // Defining celsius
+        double celsius = 30;
+        System.out.println("Celsius : "+celsius);
+
+        double Fahrenheit = (celsius * 9/5)+32;
+        System.out.println("Fahrenheit : "+Fahrenheit);
+    }
+}
