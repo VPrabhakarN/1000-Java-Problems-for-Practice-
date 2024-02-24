ee07343193acf0ffaea272aac02cbc0138cfd82f
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-02-24 09:26:38 +0530

    Add files via upload
    This is the 51th problem with the solution code.

---
+++ Q51_conversion_problem_12.java
@@ -0,0 +1,14 @@
+// Java program to Convert char to String
+
+public class Q51_conversion_problem_12 {
+    public static void main(String args[])
+    {
+        // Defining char 
+        char ch = 'v';
+        System.out.println("Char : "+ch);
+
+        // conversion
+        String str = String.valueOf(ch);
+        System.out.println("String : "+str);
+    }
+}
