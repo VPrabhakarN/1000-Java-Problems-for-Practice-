f3b784a1e5075c2ac167266ba6d88f4227ed73ee
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-02-24 18:23:34 +0530

    Add files via upload
    This is 54th problem with the solution code.

---
+++ Q54_conversion_problem_15.java
@@ -0,0 +1,14 @@
+// Java program to Convert int to double
+
+public class Q54_conversion_problem_15 {
+    public static void main(String args[])
+    {
+        // defining int 
+        int tr = 1923;
+        System.out.println("int : "+tr);
+
+        //conversion
+        float ft = tr;
+        System.out.println("float : "+ft);
+    }
+}
