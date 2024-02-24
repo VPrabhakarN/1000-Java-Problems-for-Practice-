94eb332597344780ebd4bee5b5aceec80c688ba0
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-02-24 18:16:57 +0530

    Add files via upload
    This is 53rd problem with the solution code.

---
+++ Q53_conversion_problem_14.java
@@ -0,0 +1,14 @@
+// Java Program to convert Fahrenheit into Celsius
+
+public class Q53_conversion_problem_14 {
+    public static void main(String args[])
+    {
+        // Defining Fahreneit 
+        float Fahreneit = 101;
+        System.out.println("Fahrenheit : "+Fahreneit);
+
+        // Conversion 
+        float celsius = ((Fahreneit-32)*5)/9;
+        System.out.println("Celsius : "+celsius);
+    }
+}
