259b0bee9beaef22773216e7dc48e139bdcd8805
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-02-27 08:16:20 +0530

    Add files via upload
    This is the 59th coding problem with the solution code.

---
+++ Q59_conversion_problem_20.java
@@ -0,0 +1,14 @@
+// Java program to Convert double to int
+
+public class Q59_conversion_problem_20 {
+    public static void main(String args[])
+    {
+        // Defining double 
+        double d = 1923.1923;
+        System.out.println("Double : "+d);
+
+        // conversion 
+        int i = (int)d;
+        System.out.println("Int : "+i);
+    }
+}
