4c44080533eebaeea3bd69363fd8ab48478f24d3
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-02-27 07:55:57 +0530

    Add files via upload
    This is the 57th problem with the solution code.

---
+++ Q57_conversion_problem_18.java
@@ -0,0 +1,14 @@
+// Java program to Convert boolean to String
+
+public class Q57_conversion_problem_18 {
+    public static void main(String args[])
+    {
+        // defining boolean
+        boolean bool = true;
+        System.out.println("Boolean : "+bool);
+
+        // conversion 
+        String str = String.valueOf(bool);
+        System.out.println("String : "+str);
+    }
+}
