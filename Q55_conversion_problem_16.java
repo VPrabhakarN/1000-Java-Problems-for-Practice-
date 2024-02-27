88eac23e84a3559631fd5ce0a7a9f09cb6d938ca
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-02-27 06:49:22 +0530

    Add files via upload
    This is the 55th problem with the solution code.

---
+++ Q55_conversion_problem_16.java
@@ -0,0 +1,14 @@
+// Java program to Convert String to float
+
+public class Q55_conversion_problem_16 {
+    public static void main(String args[])
+    {
+        // Defining string 
+        String str = "19.23";
+        System.out.println("String : "+str);
+
+        // conversion 
+        float f = Float.parseFloat(str);
+        System.out.println("Float : "+f);
+    }
+}
