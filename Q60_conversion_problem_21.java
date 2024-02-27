e7d0e3b6debca83a15ff1ba20686e39903d52a5e
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-02-27 08:21:00 +0530

    Add files via upload
    This is my 60th coding problem with the solution code.

---
+++ Q60_conversion_problem_21.java
@@ -0,0 +1,14 @@
+// Java program to Convert double to String
+
+public class Q60_conversion_problem_21 {
+    public static void main(String args[])
+    {
+        // Defining double 
+        double db = 1922.1233;
+        System.out.println("Double : "+db);
+
+        // conversion 
+        String str = String.valueOf(db);
+        System.out.println("String : "+str);
+    }
+}
