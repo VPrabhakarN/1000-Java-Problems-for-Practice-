8f4ae796e4b01a71c9c80bb488d5e8f98694ee58
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-02-27 08:05:53 +0530

    Add files via upload
    This is the 58th problem with the solution code.

---
+++ Q58_conversion_problem_19.java
@@ -0,0 +1,14 @@
+// Java program to Convert char to int
+
+public class Q58_conversion_problem_19 {
+    public static void main(String args[])
+    {
+        // defining char 
+        char ch = '8';
+        System.out.println("Char : "+ch);
+
+        // conversion 
+        int i = ch-'0';
+        System.out.println("Int : "+i);
+    }
+}
