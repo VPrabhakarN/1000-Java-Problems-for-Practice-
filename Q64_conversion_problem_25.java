5964c3bd6ebeaf3fae127ffc91525aeb2148fe56
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-02-28 12:27:37 +0530

    Add files via upload
    This is the 64th coding problem with the solution code.

---
+++ Q64_conversion_problem_25.java
@@ -0,0 +1,14 @@
+// Java program to Convert hours into seconds
+
+public class Q64_conversion_problem_25 {
+    public static void main(String args[])
+    {
+        // Defining hours 
+        float hrs = 3.5F;
+        System.out.println("Hours : "+hrs);
+
+        // conversion 
+        float secs = hrs*60*60;
+        System.out.println("Seconds : "+secs);
+    }
+}
