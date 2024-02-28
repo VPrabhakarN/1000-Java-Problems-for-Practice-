bcb045b0893aaf37c0ec13ff06d2e80e76307f12
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-02-28 12:42:19 +0530

    Add files via upload
    This is the 69th coding problem along with the solution code.

---
+++ Q69_conversion_problem_29.java
@@ -0,0 +1,13 @@
+// Java program to Convert temperature degree from Celsius to Kelvin
+public class Q69_conversion_problem_29 {
+    public static void main(String args[])
+    {
+        // Defining Celsius 
+        float cels = 28; // 28 degrees
+        System.out.println("Celsius : "+cels);
+
+        // Conversion 
+        float kel = cels + 273;
+        System.out.println("Kelvin : "+kel);
+    }
+}
