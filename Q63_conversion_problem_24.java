82c2ba9b995065b0df52f611bbda15df5745b732
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-02-28 12:27:14 +0530

    Add files via upload
    This is the 63rd coding problem with the solution code.

---
+++ Q63_conversion_problem_24.java
@@ -0,0 +1,15 @@
+// Java program to hours into minutes
+
+public class Q63_conversion_problem_24 {
+    public static void main(String args[])
+    {
+        // defining hours 
+        int hrs = 2; // 2 hours 
+        System.out.println("Hours : "+hrs);
+
+        // conversions 
+        int mins = hrs*60;
+        System.out.println("Minutes : "+mins);
+
+    }
+}
