4362e3449e910dd51ba587f7cf603b8e0de3200d
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-02-28 12:29:14 +0530

    Add files via upload
    This is the 67th coding problem with the solution code.

---
+++ Q67_conversion_problem_28.java
@@ -0,0 +1,14 @@
+// Program to convert seconds into minutes
+
+public class Q67_conversion_problem_28 {
+    public static void main(String args[])
+    {
+        // Defining seconds 
+        int secs = 19231923;
+        System.out.println("Seconds : "+secs);
+
+        // Conversion 
+        float mins = secs/60;
+        System.out.println("Minutes : "+mins);
+    }
+}
