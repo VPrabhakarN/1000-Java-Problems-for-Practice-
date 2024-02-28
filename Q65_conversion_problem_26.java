3c54019ed645bb5210eaca28af6a8c04f05a9fb0
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-02-28 12:27:54 +0530

    Add files via upload
    This is the 65th coding problem with the solution code.

---
+++ Q65_conversion_problem_26.java
@@ -0,0 +1,14 @@
+// Java program to Convert minutes to seconds 
+
+public class Q65_conversion_problem_26 {
+    public static void main(String args[])
+    {
+        // Defining minutes 
+        int mins = 123;
+        System.out.println("Minutes : "+mins);
+
+        // converions 
+        int secs = mins*60;
+        System.out.println("Seconds : "+secs);
+    }
+}
