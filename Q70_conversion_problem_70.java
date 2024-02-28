657ca59fbeeecfc3b377ada8d50633146a05f070
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-02-28 12:54:32 +0530

    Add files via upload
    This is the 70th problem with the solution code.

---
+++ Q70_conversion_problem_70.java
@@ -0,0 +1,15 @@
+// Java program to Convert temperature degree from Kelvin to celsius
+
+public class Q70_conversion_problem_70 {
+    public static void main(String args[])
+    {
+        // Defining kelvin 
+        float k = 23;
+        System.out.println("Kelvin : "+k);
+
+        // Conversion 
+        double c = k-273.15;
+        System.out.println("Celsius : "+c);
+
+    }
+}
