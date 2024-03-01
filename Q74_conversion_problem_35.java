cfb1bf89d7bf2495f48de37f01233cb50b25aacd
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-03-01 21:12:59 +0530

    Add files via upload
    This is the 74th problem with the solution code.

---
+++ Q74_conversion_problem_35.java
@@ -0,0 +1,14 @@
+// Java program to convert cm to Inches
+
+public class Q74_conversion_problem_35 {
+    public static void main(String args[])
+    {
+        // Definining cm 
+        float cm = 19;
+        System.out.println("Centimeter : "+cm);
+
+        // conversion 
+        double inches = cm/2.54;
+        System.out.println("Inches : "+inches);
+    }
+}
