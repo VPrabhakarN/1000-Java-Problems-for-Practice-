6c91efbc272426273e949cf8e8e69a856c684eb1
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-03-01 20:41:10 +0530

    Add files via upload
    This is the 71st problem with the solution code.

---
+++ Q71_conversion_problem_32.java
@@ -0,0 +1,14 @@
+// Java program to convert mm to cm
+
+public class Q71_conversion_problem_32 {
+    public static void main(String args[])
+    {
+        // Defining mm 
+        float mm = 11.12F;
+        System.out.println("Millimeter : "+mm);
+
+        // conversion 
+        float cm = mm/10;
+        System.out.println("Centimeter : "+cm);
+    }
+}
