4a775bf0eba5a58b2aeb473eca49dbfc9224b7ad
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-03-01 21:05:22 +0530

    Add files via upload
    This is the 73rd problem with the solution code.

---
+++ Q73_conversion_problem_34.java
@@ -0,0 +1,14 @@
+// Java program to convert cm to feet
+
+public class Q73_conversion_problem_34 {
+    public static void main(String args[])
+    {
+        // Defining cm 
+        float cm = 2;
+        System.out.println("Centimeters : "+cm);
+
+        // conversion 
+        double feet = cm / 30.48;
+        System.out.println("Feet : "+feet);
+    }
+}
