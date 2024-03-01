69f3f39136496bd197f2d56bc82a0033290f24cc
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-03-01 21:51:36 +0530

    Add files via upload
    This is the 79th java problem with the solution code.

---
+++ Q79_conversion_problem_40.java
@@ -0,0 +1,14 @@
+// Java program to convert Inches to Feet
+
+public class Q79_conversion_problem_40 {
+    public static void main(String args[])
+    {
+        // Defining inches 
+        float inches = 19;
+        System.out.println("Inches : "+inches);
+
+        // conversion 
+        double feet = inches / 12;
+        System.out.println("Feet : "+feet);
+    }
+}
