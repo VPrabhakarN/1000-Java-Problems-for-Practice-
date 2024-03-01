40b10521233c23d3e70b51070b4ff71717245213
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-03-01 21:17:17 +0530

    Add files via upload
    This is 75th problem with the solution code.

---
+++ Q75_conversion_problem_36.java
@@ -0,0 +1,14 @@
+// Java program to convert m to Feet
+
+public class Q75_conversion_problem_36 {
+    public static void main(String args[])
+    {
+        // Defining m 
+        float m = 19;
+        System.out.println("Meter : "+m);
+
+        // conversion 
+        double feet = m * 3.281;
+        System.out.println("Feet : "+feet);
+    }
+}
