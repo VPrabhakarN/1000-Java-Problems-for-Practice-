0a061aaeffc254fa58dcc722550839f295943aad
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-03-01 21:29:23 +0530

    Add files via upload
    This is the 77th problem with the solution code.

---
+++ Q77_conversion_problem_38.java
@@ -0,0 +1,14 @@
+// Java program to convert m to cm
+
+public class Q77_conversion_problem_38 {
+    public static void main(String args[])
+    {
+        // Defining m 
+        float m = 19;
+        System.out.println("Meter : "+m);
+
+        // conversion 
+        double cm = m * 100;
+        System.out.println("Centimeter : "+cm);
+    }
+}
