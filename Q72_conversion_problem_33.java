3f6a65158b3b9d0ec53dbaa07c3e627e0f74d92d
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-03-01 20:52:58 +0530

    Add files via upload
    This is the 72nd problem with the solution code.

---
+++ Q72_conversion_problem_33.java
@@ -0,0 +1,14 @@
+// Java program to convert mm to Feet
+
+public class Q72_conversion_problem_33 {
+    public static void main(String args[])
+    {
+        // Defining mm 
+        float mm = 1;
+        System.out.println("Millimeters : "+mm);
+
+        // conversion 
+        double feet = mm/304.8;
+        System.out.println("Feet : "+feet);
+    }
+}
