29ad1ad6fc60b8531d0e06539a7d0d9e0e9801f2
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-03-02 07:58:49 +0530

    Add files via upload
    This is the 82nd problem with the solution code.

---
+++ Q82_conversion_problem_43.java
@@ -0,0 +1,14 @@
+// Java program to convert feet to m
+
+public class Q82_conversion_problem_43 {
+    public static void main(String args[])
+    {
+        // Defining feet 
+        float feet = 19;
+        System.out.println("Feet : "+feet);
+
+        // Conversion 
+        double m = feet/3.281;
+        System.out.println("Meter : "+m);
+    }
+}
