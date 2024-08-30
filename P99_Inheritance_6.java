3ce7ea914e34f8f38754c51b9706cb5b6756049a
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-08-30 12:46:00 +0530

    Add files via upload

---
+++ P99_Inheritance_6.java
@@ -0,0 +1,36 @@
+// Q.99) Create a base class Appliance with a method turnOn(). Derive a class WashingMachine that overrides the turnOn() method. Demonstrate the use of the super keyword to call the base class method.
+
+
+public class P99_Inheritance_6 {
+    public static void main(String [] args){
+        // creating an object of the derived class
+        WashingMachine w1 = new WashingMachine();
+
+        // calling method to turning on the machine
+        w1.turnOn();
+    }
+}
+
+class Appliance {
+    public Appliance(){
+        System.out.println("This base class's constructor is automatically called by using super() method");
+    }
+
+    // method to turn on 
+    public void turnOn(){
+        System.out.println("This is turning on!");
+    }
+}
+
+class WashingMachine extends Appliance {
+    public WashingMachine(){
+        super();
+        System.out.println("This is washing machine! and this derived class's constructor is automatically called while object is created!");
+    }
+
+    // override the method 
+    @Override 
+    public void turnOn() {
+        System.out.println("Turning on washing machine........");
+    }
+}
