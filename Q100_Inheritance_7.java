aa570b9817d86e1ac744f458c920ca72c65b9612
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-08-30 16:38:13 +0530

    Update Q100_Inheritance_7.java

--- Q100_Inheritance_7.java
+++ Q100_Inheritance_7.java
@@ -1,6 +1,6 @@
 // Q.100)  Implement an example of constructor chaining in inheritance where a derived class constructor calls the base class constructor using super(). Use a base class Building and a derived class House
 
-public class P100_Inheritance_7 {
+public class Q100_Inheritance_7 {
     public static void main(String [] args){
         // creating an object of the derived class
         House h1 = new House();
